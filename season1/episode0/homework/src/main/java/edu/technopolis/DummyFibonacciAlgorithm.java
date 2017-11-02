ffc102e31a2b510d3431756b3afc049ac7a16897
DyominMV <32401059+DyominMV@users.noreply.github.com>
2017-11-02 17:20:16 +0300

    Update DummyFibonacciAlgorithm.java

--- season1/episode0/homework/src/main/java/edu/technopolis/DummyFibonacciAlgorithm.java
+++ season1/episode0/homework/src/main/java/edu/technopolis/DummyFibonacciAlgorithm.java
@@ -1,74 +1,80 @@
 package edu.technopolis;
 
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
-/**
- * Это <b>(ну вроде)правильный</b> с точки зрения задания алгоритм.
- */
 public class DummyFibonacciAlgorithm implements FibonacciAlgorithm {
     @Override
-    public String evaluate(int index) {
+    public static String evaluate(int index) throws Exception  {
         String st="";
-        if (index<0 && (-index)%2==0)
-            st+="-";
-        index=Math.abs(index);
-        st+=fibIter(index, index / 64+ 1).toString();
-        return st;
+        if (index<=0){
+            throw new Exception ("Invalid index");
+        } else {
+            return fibIter(index, index / 64 + 1).toString();
+        }
     }
 
-    public static BigNumber fibIter(long n, int count){
-        if (n==0) return new BigNumber(count, 0L); else
-        if (n==1 || N==2) return new BigNumber(count, 1L); else{
-            BigNumber N1 = new BigNumber(count, 1L);
-            BigNumber N2 = new BigNumber(count, 1L);
+    private static BigNumber fibIter(long n, int count){
+        if (n==0){
+            return new BigNumber(count, 0L);
+        } else if (n==1 || n==2) {
+            return new BigNumber(count, 1L);
+        } else{
+            BigNumber n1 = new BigNumber(count, 1L);
+            BigNumber n2 = new BigNumber(count, 1L);
             for (long i=3; i<=n; i+=2){
-                N1 = BigNumber.Summ(N1,N2);
-                N2 = BigNumber.Summ(N1,N2);
+                n1 = BigNumber.Summ(n1,n2);
+                n2 = BigNumber.Summ(n1,n2);
             }
-            if (N % 2==0) return N2; else return N1;
+            if (n % 2==0){
+                return n2;
+            } else {
+                return n1;
+            }
         }
     }
 }
 
 class BigNumber{
     public int c;
-    public long[] Longs;
+    public long[] longs;
 
-    public BigNumber(int Count,long d){
-        Longs = new long[Count];
-        c = Count;
-        for (int x=0; x<Count; x++)
-            Longs[x] = 0;
-        Longs[Count-1] = d;
+    public BigNumber(int count,long d){
+        longs = new long[count];
+        c = count;
+        for (int x=0; x<count; x++){
+            longs[x] = 0;
+        }
+        longs[count-1] = d;
     }
 
     public String toString(){
-        java.math.BigDecimal D = new java.math.BigDecimal(0);
-        D = D.add(new java.math.BigDecimal((Longs[0])>>>1));
-        D = D.add(new java.math.BigDecimal((Longs[0])>>>1));
-        D = D.add(new java.math.BigDecimal(Longs[0]&1));
+        BigDecimal d = new BigDecimal(0);
+        d = d.add(new java.math.BigDecimal((longs[0])>>>1));
+        d = d.add(new java.math.BigDecimal((longs[0])>>>1));
+        d = d.add(new java.math.BigDecimal(longs[0]&1));
         for (int x=1; x<c; x++){
-            D = D.multiply(new java.math.BigDecimal(1L<<32));
-            D = D.multiply(new java.math.BigDecimal(1L<<32));
-            D = D.add(new java.math.BigDecimal((Longs[x])>>>1));
-            D = D.add(new java.math.BigDecimal((Longs[x])>>>1));
-            D = D.add(new java.math.BigDecimal(Longs[x]&1));
+            d = d.multiply(new java.math.BigDecimal(1L<<32));
+            d = d.multiply(new java.math.BigDecimal(1L<<32));
+            d = d.add(new java.math.BigDecimal((longs[x])>>>1));
+            d = d.add(new java.math.BigDecimal((longs[x])>>>1));
+            d = d.add(new java.math.BigDecimal(longs[x]&1));
         }
-        return D.toString();
+        return d.toString();
     }
 
-    public static boolean Bigger(long L1, long L2){
-        return Long.compare(L1^0x8000000000000000L, L2^0x8000000000000000L)>0;
+    public static boolean Bigger(long l1, long l2){
+        return Long.compare(l1^0x8000000000000000L, l2^0x8000000000000000L)>0;
     }
 
-    public static BigNumber Summ(BigNumber N1, BigNumber N2){
-        BigNumber N = new BigNumber(N1.C, 0L);
-        N.Longs[N.c-1]=(long)(N1.Longs[N.c-1]+N2.Longs[N.c-1]);
-        for (char a=(char)(N.c-1); a >0; a--){
-            N.Longs[a-1] = (long)(N1.Longs[a-1]+N2.Longs[a-1]);
-            if (Bigger(N1.Longs[a],N.Longs[a])||Bigger(N2.Longs[a],N.Longs[a]))
-                N.Longs[a-1]++ ;
+    public static BigNumber Summ(BigNumber n1, BigNumber n2){
+        BigNumber n = new BigNumber(n1.c, 0L);
+        n.longs[n.c-1] = (n1.longs[n.c-1] + n2.longs[n.c-1]);
+        for (int a=(n.c-1); a >0; a--){
+            n.longs[a-1] = (n1.longs[a-1] + n2.longs[a-1]);
+            if (Bigger(n1.longs[a], n.longs[a]) || Bigger(n2.longs[a], n.longs[a])) {
+                n.longs[a - 1]++;
+            }
         }
-        return N;
+        return n;
     }
 }
