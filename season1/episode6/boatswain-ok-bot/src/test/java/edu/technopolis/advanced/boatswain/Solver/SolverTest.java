4b29d0ba4d382a3d5b75ce8a50b354a73cb76bce
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2017-12-15 00:39:58 +0300

    decimal-test

--- season1/episode6/boatswain-ok-bot/src/test/java/edu/technopolis/advanced/boatswain/Solver/SolverTest.java
+++ season1/episode6/boatswain-ok-bot/src/test/java/edu/technopolis/advanced/boatswain/Solver/SolverTest.java
@@ -14,8 +14,12 @@ public class SolverTest {
 
     @Test
     public void evaluate1() throws Exception {
-        System.out.println(Math.cos(3.14)+(1-3) / Math.sin(1.0-2.2+Math.tan(0.7333)));
         assertEquals(Math.cos(3.14)+(1-3) / Math.sin(1.0-2.2+Math.tan(0.7333)), solver.evaluate("cos(3.14)+(1-3)/sin(1.0-2.2+tg(0.7333))").doubleValue(), 0.01);
+    }
+
+    @Test
+    public void evaluate2() throws Exception {
+        assertEquals(1-0.1-0.1-0.1-0.1, solver.evaluate("1-0.1-0.1-0.1-0.1").doubleValue(), 0.01);
     }
 
 }
