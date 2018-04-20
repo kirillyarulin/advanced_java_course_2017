d557a1b9dcc45cdb02a4a05531d4ca760dde623c
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2018-04-20 19:21:58 +0300

    done

--- season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
+++ season2/episode3/src/main/java/edu/technopolis/advancedjava/Deadlock.java
@@ -3,7 +3,8 @@ package edu.technopolis.advancedjava;
 public class Deadlock {
     private static final Object FIRST_LOCK = new Object();
     private static final Object SECOND_LOCK = new Object();
-
+    private static final Object t = new Object();
+    static int state=0;
     public static void main(String[] args) throws Exception {
         Thread ft = new Thread(Deadlock::first);
         Thread st = new Thread(Deadlock::second);
@@ -14,24 +15,47 @@ public class Deadlock {
         //never going to reach this point
     }
 
-    private static void first() {
+
+
+
+    private static void first()  {
         synchronized(FIRST_LOCK) {
             //insert some code here to guarantee a deadlock
+
+           synchronized (t)
+           {
+               state++;
+               while (state<2){
+                   try {
+                       t.wait();
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+               }
+               t.notifyAll();
+           }
             synchronized(SECOND_LOCK) {
                 //unreachable point
             }
         }
     }
 
-    private static void second() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            //
-        }
+    private static void second()  {
         //reverse order of monitors
         synchronized(SECOND_LOCK) {
-            //insert some code here to guarantee a deadlock
+            synchronized (t)
+            {
+                state++;
+                while (state<2){
+                    try {
+                        t.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                t.notifyAll();
+            }
+
             synchronized(FIRST_LOCK) {
                 //unreachable point
             }
